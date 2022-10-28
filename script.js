764b221ec449b71c000dc294fe3315529e0cee2a
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2022-10-28 11:17:43 -0700

    recommended chages for starting repo

--- script.js
+++ script.js
@@ -1,164 +1,90 @@
-// This line allows us to bring in the data object from our data.js file
-const data = window.data;
+// Need to use the window object to get access to our data from data.js
 
-// Make your References to the two DOM nodes
+// Make your references to the your DOM nodes here as you need them
 
-// Create a reference to the element who's ID is 'big_coffee and call it bigCoffee
-
-// Create a reference to the element who's ID is 'producer_container' and call it producerContainer
 
 /**************
  *   SLICE 1
  **************/
 
-function updateCoffeeView(coffeeQty) {
-  // Create a reference to the element who's ID is 'coffee_counter'
-  // Set the innerText of that element to be the coffeeQty passed into this function
-}
+// Create a render view function for your counter
+  // Create a reference to your counter element
+  // Set the innerText of that element to be the resource quantity
 
-function clickCoffee(data) {
-  // Increment the data object's (passed into this function) coffee property by one
-  // call the updateCoffeeView function and pass it the newly updated data.coffee property
-  // call the renderProducers function and pass it the data object
-}
+// Create a function to handle what happens when the user clicks the "clicker" element
+  // Increment the data object's resource property by one
+  // call the render view function and pass it the newly updated resource
+  // call the render producers function and pass it the data object 
+    // need to create the render producers function first in slice 2
 
 /**************
  *   SLICE 2
  **************/
 
-function unlockProducers(producers, coffeeCount) {
+// create a function to render the producers
+  // call the unlock producers function
+    // default to all producers and then create the unlock producers function
+  // call the delete all child nodes function and pass it the producer container element
+    // need to create a function to delete all the child nodes of a passed in container
+  // call the function to get all the unlocked producers so that we can display them
+    // need to create this function
+  // create a function that creates a div for each producer
+  // loop through the unlocked producers
+    // call appendChild on the producer container for each producer
+
+// create a function that goes through the producers and sets isLocked
   // loop through the producers array passed into the function
-  // for each producer, if the coffeeCount (passed in) is greater than or equal
-  // to half the producer's price, reassign the producers.unlocked property to equal true
-}
+  // for each producer, if the resource count (passed in) is greater than or equal
+    // to half the producer's price, reassign the producers.unlocked property to equal true
 
-function getUnlockedProducers(data) {
+// create a function that deletes all of the child nodes of a given container
+
+// create a function that gets all of the unlocked producers
   // use the Array.prototype.filter() method
-  // filter through the data.producers property, and return an array with only the producers whose
-  // unlocked property is true
-}
+  // filter through the producers and return an array with only the producers whose
+    // unlocked property is true
 
-// You do not need to edit this function
-function makeDisplayNameFromId(id) {
-  return id
-    .split("_")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-}
-
-// You do not need to edit this function
-function makeProducerDiv(producer) {
-  const containerDiv = document.createElement("div");
-  containerDiv.className = "producer";
-  const displayName = makeDisplayNameFromId(producer.id);
-  const currentCost = producer.price;
-  const html = `
-    <div class="producer-column">
-      <div class="producer-title">${displayName}</div>
-      <button type="button" id="buy_${producer.id}">Buy</button>
-    </div>
-    <div class="producer-column">
-      <div>Quantity: ${producer.qty}</div>
-      <div>Coffee/second: ${producer.cps}</div>
-      <div>Cost: ${currentCost} coffee</div>
-    </div>
-    `;
-  containerDiv.innerHTML = html;
-  return containerDiv;
-}
-
-// You do not need to edit this function
-function deleteAllChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
-}
-
-function renderProducers(data) {
-  // call the unlockProducers function and pass it data.producers and data.coffee
-
-  // make a reference to the DOM element whose ID is producer_container
-
-  // call the deleteAllChildNodes function and pass it the above producerContainer element
-
-  // you do not need to edit the following code, but for understanding, this gets the unlocked producers,
-  // and for each producer makes a little html div with that producer's info
-  getUnlockedProducers(data).forEach((producer) => {
-    producerContainer.appendChild(makeProducerDiv(producer));
-  });
-}
+// create a function that creates a div for a given producer
 
 /**************
  *   SLICE 3
  **************/
 
-// You do not need to edit this function
-function getProducerById(data, producerId) {
-  return data.producers.find((producer) => producerId === producer.id);
-}
+// create a buy button click function
+  // if the target is a button
+    // attempt to buy the producer
+      // need to create the function to attempt to buy the producer
+    // if they can't buy the producer
+      // tell the user they do not have enough resources
+    // else
+      // call the render producers function
+      // call the update resource view function
+      // call the update CPS view function
+        // need to create this function
 
-// You do not need to edit this function
-function canAffordProducer(data, producerId) {
-  return getProducerById(data, producerId).price <= data.coffee;
-}
+// create the attempt to buy producer function
+  // if they can afford the producer
+    // reduce the amount of resources they have by the price of the producer
+    // increase the producer quantity by 1
+    // update the price of the producer to something more expensive (maybe x1.25)
+    // update the totalCPS
+  // return true or false based on if the user could afford the producer
 
-// You do not need to edit this function
-function updateCPSView(cps) {
-  const cpsDiv = document.getElementById("cps");
-  cpsDiv.innerText = cps;
-}
+// create a function to update the CPS view
+  // use the cps element to change the inner text to the new cps
 
-// You do not need to edit this function
-function updatePrice(oldPrice) {
-  return Math.floor(oldPrice * 1.25);
-}
+// create a tick function
+  // increment the data object's resource by the totalCPS
+  // call the update resource view function
+  // call the render producers function
 
-// You do not need to edit this function
-function attemptToBuyProducer(data, producerId) {
-  if (canAffordProducer(data, producerId)) {
-    const producer = getProducerById(data, producerId);
-    data.coffee -= producer.price;
-    producer.qty += 1;
-    producer.price = updatePrice(producer.price);
-    data.totalCPS += producer.cps;
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// You do not need to edit this function
-function buyButtonClick(event, data) {
-  if (event.target.tagName === "BUTTON") {
-    const producerId = event.target.id.slice(4);
-    const result = attemptToBuyProducer(data, producerId);
-    if (!result) {
-      window.alert("Not enough coffee!");
-    } else {
-      renderProducers(data);
-      updateCoffeeView(data.coffee);
-      updateCPSView(data.totalCPS);
-    }
-  }
-}
-
-function tick(data) {
-  // increment the data object's (passed into this function)
-  // coffee property by the data.totalCPS amount
-
-  // call the updateCoffeeView function and pass it the data.coffee property
-
-  // call the renderProducers function and pass it the newly updated data object
-  renderProducers(data);
-}
 
 // Event Listeners
 
-// add a 'click' event listener to the bigCoffee element (that you referenced above)
-// the event listener should call the clickCoffee function, and pass in the global data object
+// add a 'click' event listener to the "clicker" element (that you referenced above)
+// the event listener should call the click function
 
-// add a 'click' event listener to the element (referenced at the top of the file)
-// the event listener should call the buyButtonClick function and pass it the event, and the global data object
+// add a 'click' event listener to the buy element (referenced at the top of the file)
+// the event listener should call the buy function
 
-// You do not need to edit this last line. This simple runs your tick function every 1000ms, or 1s
-setInterval(() => tick(data), 1000);
+// create a setInterval that calls the tick function every second
